4c7da3073ac71f0d2ab788fe2fd8e7922ba64ee9
leonardo-berlatto <leoberlatto@gmail.com>
2020-08-02 21:08:12 -0300

    ✨ Adding timer component

---
+++ components/timer/Timer.style.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window')
+
+
+export default StyleSheet.create({
+    timerText: {
+        color: '#fff',
+        fontSize: 60
+    },
+    progressBar: {
+        width: height/3,
+        height: height/3,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 1000,
+        borderColor: '#4630eb',
+        borderWidth: 30,
+        marginVertical: 20
+    }
+})
---
+++ components/timer/Timer.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import styles from './Timer.style'
+
+export interface TimerProps {
+    time: number;
+}
+
+
+export function Timer({ time }: TimerProps) {
+
+    // TODO: refact to helper/formatter
+    const renderTime = (numberOfSeconds: number) => {
+        const timeLeft = {
+            minutes: Math.floor((numberOfSeconds / 60) % 60),
+            seconds: Math.floor(numberOfSeconds % 60)
+        };
+
+        return `${("0" + timeLeft.minutes).slice(-2)}:${("0" + timeLeft.seconds).slice(-2)}`;
+    }
+
+    return (
+        <View style={styles.progressBar}>
+            <Text style={styles.timerText}>{renderTime(time)}</Text>
+        </View>
+    )
+}
