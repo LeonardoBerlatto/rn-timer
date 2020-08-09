a9a4b46d68d478fa5e4767aaa5f011452e8a0a26
leonardo-berlatto <leoberlatto@gmail.com>
2020-08-08 22:15:33 -0300

    🎉 Making timer works

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import { registerRootComponent } from 'expo';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Timer } from './components';
 import { TimerActions } from './sections';
 
 export default function App() {
+  const [timeInSeconds, setTimeInSeconds] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    let interval = 0;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setTimeInSeconds(timeInSeconds => timeInSeconds + 1);
+      }, 1000);
+    } else if (!isPlaying && timeInSeconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isPlaying, timeInSeconds]);
+
+  const pause = () => {
+    setIsPlaying(false);
+  }
+
+  const stop = () => {
+    setTimeInSeconds(0);
+    setIsPlaying(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Timer time={100} />
+      <Timer time={timeInSeconds} />
       <TimerActions
-        onPlayCallback={() => { }}
-        onPauseCallback={() => { }}
-        onStopCallback={() => { }}
-        isPlaying={true} />
+        timeInSeconds={timeInSeconds}
+        onPlayCallback={() => { setIsPlaying(true) }}
+        onPauseCallback={pause}
+        onStopCallback={stop}
+        isPlaying={isPlaying} />
       <View style={styles.test}>
       </View>
     </View>
--- src/sections/timer-actions/timer-actions.section.tsx
+++ src/sections/timer-actions/timer-actions.section.tsx
@@ -5,30 +5,57 @@ import styles from './timer-actions.style';
 
 interface TimerActionsProps {
     isPlaying: boolean,
+    timeInSeconds: number,
     onPlayCallback(): void,
     onPauseCallback(): void,
     onStopCallback(): void;
 }
 
-export function TimerActions({ isPlaying, onPlayCallback, onPauseCallback, onStopCallback }: TimerActionsProps) {
+export function TimerActions({ isPlaying, timeInSeconds, onPlayCallback, onPauseCallback, onStopCallback }: TimerActionsProps) {
 
+    const renderPlaying = () => {
+        return (
+            <>
+                <TouchableOpacity style={styles.actionButton} onPress={onStopCallback}>
+                    <Text style={styles.actionButtonText}>Stop</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.actionButton} onPress={onPauseCallback}>
+                    <Text style={styles.actionButtonText}>pause</Text>
+                </TouchableOpacity>
+            </>
+        )
+    }
+
+    const renderStopped = () => {
+        return (
+            <TouchableOpacity style={styles.actionButton} onPress={onPlayCallback}>
+                <Text style={styles.actionButtonText}>play</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    const renderPaused = () => {
+        return (
+            <>
+                <TouchableOpacity style={styles.actionButton} onPress={onStopCallback}>
+                    <Text style={styles.actionButtonText}>stop</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.actionButton} onPress={onPlayCallback}>
+                    <Text style={styles.actionButtonText}>play</Text>
+                </TouchableOpacity>
+            </>
+        )
+    }
 
     const renderButtons = () => {
-        if (!isPlaying) {
-            return (
-                <>
-                    <TouchableOpacity style={styles.actionButton} onPress={onStopCallback}>
-                        <Text style={styles.actionButtonText}>Stop</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.actionButton} onPress={onPauseCallback}>
-                        <Text style={styles.actionButtonText}>pause</Text>
-                    </TouchableOpacity>
-                </>
-            )
+        if (isPlaying) {
+            return renderPlaying();
         }
-        return <TouchableOpacity style={styles.actionButton} onPress={onPlayCallback}>
-            <Text style={styles.actionButtonText}>play</Text>
-        </TouchableOpacity>
+
+        if (timeInSeconds > 0) {
+            return renderPaused();
+        }
+        return renderStopped();
     }
 
     return (
